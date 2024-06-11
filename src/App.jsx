8d46ef824727c2d6acb2a8fcc1163c03900be9e6
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-06-11 14:48:16 +0530

    chore: install and play with ReactFlow library by creating a flow, nodes and edges

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import React from "react";
+import ReactFlow, { Background, Controls } from "reactflow";
+import "reactflow/dist/style.css";
+
+const nodes = [
+  { id: "1", data: { label: "source" }, position: { x: -400, y: 50 } },
+  { id: "2", data: { label: "destination" }, position: { x: 400, y: 50 } },
+];
+
+const edges = [{ id: "1-2", source: "1", target: "2" }];
+
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="h-screen w-screen ">
+      <ReactFlow nodes={nodes} edges={edges}>
+        <Controls />
+        <Background />
+      </ReactFlow>
+    </div>
+  );
 };
 
 export default App;
