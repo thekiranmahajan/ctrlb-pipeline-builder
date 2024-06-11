5f4c857aa535368e6d2e3fbdb35688fbfbdc6608
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-06-11 15:35:27 +0530

    feat: create 2 interactive nodes with node adding and dynamic placement features

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,53 @@
 import React from "react";
-import ReactFlow, { Background, Controls } from "reactflow";
+import { useCallback } from "react";
+import { useState } from "react";
+import ReactFlow, {
+  Background,
+  Controls,
+  addEdge,
+  applyEdgeChanges,
+  applyNodeChanges,
+} from "reactflow";
 import "reactflow/dist/style.css";
 
-const nodes = [
-  { id: "1", data: { label: "source" }, position: { x: -400, y: 50 } },
+const initialNodes = [
+  {
+    id: "1",
+    data: { label: "source" },
+    position: { x: -400, y: 50 },
+    type: "input",
+  },
   { id: "2", data: { label: "destination" }, position: { x: 400, y: 50 } },
 ];
 
-const edges = [{ id: "1-2", source: "1", target: "2" }];
+const initialEdges = [];
 
 const App = () => {
+  const [nodes, setNodes] = useState(initialNodes);
+  const [edges, setEdges] = useState(initialEdges);
+
+  const onNodesChange = useCallback(
+    (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
+    []
+  );
+  const onEdgesChange = useCallback(
+    (changes) => setEdges((eds) => applyEdgeChanges(changes, eds)),
+    []
+  );
+
+  const onConnect = useCallback((params) =>
+    setEdges((eds) => addEdge(params, eds))
+  );
   return (
-    <div className="h-screen w-screen ">
-      <ReactFlow nodes={nodes} edges={edges}>
+    <div className="h-screen w-screen">
+      <ReactFlow
+        nodes={nodes}
+        onNodesChange={onNodesChange}
+        edges={edges}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        fitView
+      >
         <Controls />
         <Background />
       </ReactFlow>
